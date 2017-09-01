78e75506b7d7a3f276d50f19623f364a5de24d90
AtomicArtem <31529198+AtomicArtem@users.noreply.github.com>
2017-09-01 00:57:20 -0400

    Uploaded gitignore
    Assets and ProjectSettings should be the only ones here. Rest should be autogenerated by Unity

---
+++ RollABall/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+    public float accelerationMultiplier = 20;
+    public Text countText;
+    public Text winText;
+
+    private Rigidbody rb;
+    private int count;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+    }
+
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0F, moveVertical);
+
+        rb.AddForce(movement * accelerationMultiplier);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+        }
+    }
+
+    void SetCountText ()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 12)
+            winText.text = "You've Collected all the pickups. Gratz! Do some other stuff!";
+    }
+}
---
+++ RollABall/Assets/Scripts/Rotator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+
+    public static int xRot = 15, yRot = 30, zRot = 45;
+
+    Vector3 rotationV = new Vector3(xRot, yRot, zRot);
+
+	void Update () {
+        transform.Rotate(rotationV * Time.deltaTime);
+	}
+}
